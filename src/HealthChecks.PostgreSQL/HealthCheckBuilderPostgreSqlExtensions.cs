f4b05230c8e0dc38401c63e778652ad25c7d658d
Sebastian Indrunas <sindrunas@cardinalsystems.com.ar>
2018-10-08 15:09:14 -0300

    Added HealthCheck PostgreSQL extension

---
+++ src/HealthChecks.PostgreSQL/HealthCheckBuilderPostgreSqlExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.HealthChecks;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace HealthChecks.PostgreSQL
+{
+    public static class HealthCheckBuilderPostgreSqlExtensions
+    {
+        public static HealthCheckBuilder AddPostgreSqlCheck(this HealthCheckBuilder builder, string name, string connectionString)
+        {
+            Guard.ArgumentNotNull(nameof(builder), builder);
+
+            return AddPostgreSqlCheck(builder, name, connectionString, builder.DefaultCacheDuration);
+        }
+
+        public static HealthCheckBuilder AddPostgreSqlCheck(this HealthCheckBuilder builder, string name, string connectionString, TimeSpan cacheDuration)
+        {
+            builder.AddCheck($"PostgreSqlCheck({name})", async () =>
+            {
+                try
+                {
+                    //TODO: There is probably a much better way to do this.
+                    using (var connection = new NpgsqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        using (var command = connection.CreateCommand())
+                        {
+                            command.CommandType = CommandType.Text;
+                            command.CommandText = "SELECT 1";
+                            var result = (int)await command.ExecuteScalarAsync().ConfigureAwait(false);
+                            if (result == 1)
+                            {
+                                return HealthCheckResult.Healthy($"PostgreSqlCheck({name}): Healthy");
+                            }
+
+                            return HealthCheckResult.Unhealthy($"PostgreSqlCheck({name}): Unhealthy");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return HealthCheckResult.Unhealthy($"PostgreSqlCheck({name}): Exception during check: {ex.GetType().FullName}");
+                }
+            }, cacheDuration);
+
+            return builder;
+        }
+    }
+}
